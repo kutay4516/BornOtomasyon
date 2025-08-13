cdc560ea06f9eb6356792cdc307d16582f847915
Kutay Caliskan <skykutay@gmail.com>
2025-08-13 16:39:17 +0300

    Deploy frontend to GitHub Pages

--- Backend/BornOtomasyon.API/obj/Release/net8.0/win-x64/BornOtomasyon.API.AssemblyInfo.cs
+++ Backend/BornOtomasyon.API/obj/Release/net8.0/win-x64/BornOtomasyon.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BornOtomasyon.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008364e289e1758d634c80889cee5601fb9cfc2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BornOtomasyon.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BornOtomasyon.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
